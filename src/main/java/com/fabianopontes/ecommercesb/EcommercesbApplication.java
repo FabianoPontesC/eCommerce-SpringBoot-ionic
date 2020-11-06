70fe23e775326032ff08220837d82d4fbe577643
Fabiano Pontes <fabiano.pontesc@gmail.com>
2020-11-06 19:48:00 -0300

    Project created

---
+++ src/main/java/com/fabianopontes/ecommercesb/EcommercesbApplication.java
@@ -0,0 +1,13 @@
+package com.fabianopontes.ecommercesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercesbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercesbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabianopontes/ecommercesb/EcommercesbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabianopontes.ecommercesb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercesbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
