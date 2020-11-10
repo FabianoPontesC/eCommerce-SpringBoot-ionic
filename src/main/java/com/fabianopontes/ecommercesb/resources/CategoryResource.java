ca5eb500688ec6df3918ec741c5e551f48e32a6d
Fabiano Pontes <fabiano.pontesc@gmail.com>
2020-11-10 19:55:35 -0300

    Testing first domain class

--- src/main/java/com/fabianopontes/ecommercesb/resources/CategoryResource.java
+++ src/main/java/com/fabianopontes/ecommercesb/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.fabianopontes.ecommercesb.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fabianopontes.ecommercesb.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "REST is working";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
