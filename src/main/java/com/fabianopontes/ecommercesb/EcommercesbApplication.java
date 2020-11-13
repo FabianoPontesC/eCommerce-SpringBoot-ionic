91d6d8b1580de2443e2cc986efe3d108bac212b5
Fabiano Pontes <fabiano.pontesc@gmail.com>
2020-11-13 15:59:48 -0300

    Creating instantiation operation

--- src/main/java/com/fabianopontes/ecommercesb/EcommercesbApplication.java
+++ src/main/java/com/fabianopontes/ecommercesb/EcommercesbApplication.java
@@ -1,13 +1,32 @@
 package com.fabianopontes.ecommercesb;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EcommercesbApplication {
+import com.fabianopontes.ecommercesb.domain.Category;
+import com.fabianopontes.ecommercesb.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class EcommercesbApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommercesbApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
