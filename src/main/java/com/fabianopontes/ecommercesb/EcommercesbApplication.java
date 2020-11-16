c0dce240df619859b6f60b11ffdb3b9738e0b112
Fabiano Pontes <fabiano.pontesc@gmail.com>
2020-11-16 19:45:47 -0300

    Uf and States

--- src/main/java/com/fabianopontes/ecommercesb/EcommercesbApplication.java
+++ src/main/java/com/fabianopontes/ecommercesb/EcommercesbApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.fabianopontes.ecommercesb.domain.Category;
+import com.fabianopontes.ecommercesb.domain.City;
 import com.fabianopontes.ecommercesb.domain.Product;
+import com.fabianopontes.ecommercesb.domain.Uf;
 import com.fabianopontes.ecommercesb.repositories.CategoryRepository;
+import com.fabianopontes.ecommercesb.repositories.CityRepository;
 import com.fabianopontes.ecommercesb.repositories.ProductRepository;
+import com.fabianopontes.ecommercesb.repositories.UfRepository;
 
 @SpringBootApplication
 public class EcommercesbApplication implements CommandLineRunner {
@@ -19,6 +23,10 @@ public class EcommercesbApplication implements CommandLineRunner {
 	private CategoryRepository categoryRepository;
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private UfRepository ufRepository;
+	@Autowired
+	private CityRepository cityRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommercesbApplication.class, args);
@@ -41,8 +49,21 @@ public class EcommercesbApplication implements CommandLineRunner {
 		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategories().addAll(Arrays.asList(cat1));
 		
+		Uf uf1 = new Uf(null, "MG");
+		Uf uf2 = new Uf(null, "SP");
+		
+		City c1 = new City(null, "Uberlândia", uf1);
+		City c2 = new City(null, "São Paulo", uf2);
+		City c3 = new City(null, "Campinas", uf2);
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		uf1.getCities().addAll(Arrays.asList(c1));
+		uf2.getCities().addAll(Arrays.asList(c2, c3));
+		
+		ufRepository.saveAll(Arrays.asList(uf1, uf2));
+		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
