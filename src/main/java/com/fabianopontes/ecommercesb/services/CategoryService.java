964c777f50bb41207782f9435c596f407ae565b3
unknown <fabiano.pontesc@gmail.com>
2020-11-24 12:27:03 -0300

    Deleting a Category with DELETE verb

--- src/main/java/com/fabianopontes/ecommercesb/services/CategoryService.java
+++ src/main/java/com/fabianopontes/ecommercesb/services/CategoryService.java
@@ -3,10 +3,12 @@ package com.fabianopontes.ecommercesb.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.fabianopontes.ecommercesb.domain.Category;
 import com.fabianopontes.ecommercesb.repositories.CategoryRepository;
+import com.fabianopontes.ecommercesb.services.exceptions.DataIntegrityException;
 import com.fabianopontes.ecommercesb.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,18 +18,27 @@ public class CategoryService {
 	private CategoryRepository repo;
 
 	public Category find(Integer id) {
-		 Optional<Category> obj = repo.findById(id);
+		Optional<Category> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		 "Object not found! Id: " + id + ", Type: " + Category.class.getName()));
-	} 
-	
+				"Object not found! Id: " + id + ", Type: " + Category.class.getName()));
+	}
+
 	public Category insert(Category obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Category update(Category obj) {
 		find(obj.getId());
 		return repo.save(obj);
-	}	
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("You can't delete a category with products");
+		}
+	}
 }
