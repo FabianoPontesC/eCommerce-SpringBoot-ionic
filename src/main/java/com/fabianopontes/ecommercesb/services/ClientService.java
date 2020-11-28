38d4e25b32c96357436ce764ff3f4241e6b9ea76
unknown <fabiano.pontesc@gmail.com>
2020-11-28 07:23:30 -0300

    PUT, DELETE and GET for Clients

--- src/main/java/com/fabianopontes/ecommercesb/services/ClientService.java
+++ src/main/java/com/fabianopontes/ecommercesb/services/ClientService.java
@@ -1,12 +1,19 @@
 package com.fabianopontes.ecommercesb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fabianopontes.ecommercesb.domain.Client;
+import com.fabianopontes.ecommercesb.dto.ClientDTO;
 import com.fabianopontes.ecommercesb.repositories.ClientRepository;
+import com.fabianopontes.ecommercesb.services.exceptions.DataIntegrityException;
 import com.fabianopontes.ecommercesb.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,5 +26,38 @@ public class ClientService {
 		 Optional<Client> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		 "Object not found! Id: " + id + ", Type: " + Client.class.getName()));
-		} 	
+	}
+	
+	public Client update(Client obj) {
+		Client newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("You can't delete because there are relationship entities");
+		}
+	}
+	
+	public List<Client> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Client> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Client fromDTO(ClientDTO objDto) {
+		return new Client(objDto.getId(), objDto.getName(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Client newObj, Client obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
 }
